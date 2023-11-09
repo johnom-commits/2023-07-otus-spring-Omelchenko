fb1b8b2af18228e54d02e2bc68c1dd6905893da3
johnom <johnom2012@yandex.ru>
2023-11-09 10:22:29 +0300

    Task 06. Fix warnings

--- task06-spring-orm/src/main/java/ru/otus/homework/domain/Comment.java
+++ task06-spring-orm/src/main/java/ru/otus/homework/domain/Comment.java
@@ -1,7 +1,15 @@
 package ru.otus.homework.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
